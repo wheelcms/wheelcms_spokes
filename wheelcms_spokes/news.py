7b5c666a87a8a85a7a435ebb872fd2ffdacd5c50
Ivo van der Wijk <github@in.m3r.nl>
2013-03-13 15:42:18 +0100

    add tinymce to news body

--- wheelcms_spokes/news.py
+++ wheelcms_spokes/news.py
@@ -1,16 +1,25 @@
 from django.db import models
+from django import forms
+
+from tinymce.widgets import TinyMCE
+
 from wheelcms_axle.content import type_registry, Content
 from wheelcms_axle.templates import template_registry
 from wheelcms_axle.spoke import Spoke
+from wheelcms_axle.forms import formfactory
+
 
 class News(Content):
     """ A news object """
     intro = models.TextField(blank=False)
     body = models.TextField(blank=False)
 
+class NewsForm(formfactory(News)):
+    body = forms.CharField(widget=TinyMCE(), required=False)
 
 class NewsType(Spoke):
     model = News
+    form = NewsForm
 
     title = "A simple News item"
 
