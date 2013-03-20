f531e33e365ea615ef9f14eb3df388ea2a85b6c2
Ivo van der Wijk <github@in.m3r.nl>
2013-03-20 17:35:51 +0100

    implement custom indexview for page

--- wheelcms_spokes/page.py
+++ wheelcms_spokes/page.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django import forms
 
+from django.utils.html import strip_tags
+from django.utils.text import Truncator
+
 from wheelcms_axle.content import type_registry
 from wheelcms_axle.content import Content
 from wheelcms_axle.templates import template_registry
-from wheelcms_axle.spoke import Spoke
+from wheelcms_axle.spoke import Spoke, indexfactory, SpokeCharField
 from wheelcms_axle.forms import formfactory
 
 from tinymce.widgets import TinyMCE
@@ -36,6 +39,22 @@ class PageType(Spoke):
             return "folder_page.png"
         return "page.png"
 
+    def index_description(self):
+        """ truncate body text if no explicit description available """
+        if self.instance.description:
+            return self.instance.description
+
+        return Truncator(strip_tags(self.instance.body)).words(50,
+                         truncate=" ...")
+
+    @classmethod
+    def index(cls):
+        class PageIndex(indexfactory(cls)):
+            description = SpokeCharField(spoke=cls,
+                                         stored=True, indexed=False,
+                                         model_attr="index_description")
+
+        return PageIndex
 
 type_registry.register(PageType)
 template_registry.register(PageType, "wheelcms_spokes/page_view.html",
